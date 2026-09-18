42a71ef8d3c6e4a7ac07ddf72a64bb515cfbf656
m0wmt <nobody@example.com>
2026-09-18 13:49:52 +0100

    New speedometer inspired display

--- src/main.cpp
+++ src/main.cpp
@@ -36,10 +36,17 @@ Build options:
 
 #define DEBUG false      // to turn on/off printf statements
 
-void draw_logo(void);
-void draw_display(void);
+// void draw_logo(void);
+// void draw_display(void);
+
 void esp_now_data_recv(const esp_now_recv_info_t *info, const uint8_t *incoming_data, int len);
 static uint16_t update_status(const uint8_t status);
+
+// New view for admin viewer - inspired from a car's speedometer
+void draw_speedometer_display(void);    
+void draw_diskspace(void);
+void draw_recordings(void);
+void draw_status_icon(void);
 
 // Library used only to create sprite for GFX library to draw
 TFT_eSPI tft = TFT_eSPI();
@@ -97,6 +104,8 @@ static const char* LMK_KEY_STR = LMK
 
 // End ESP-NOW
 
+#define RGB565_BACKGROUND RGB565(42, 52, 57)
+
 #define HEARTBEAT 120000                // 2 minutes
 static uint32_t inactive_timer = 0;     // inactivity run time timer
 static uint8_t counter = 0;
@@ -141,7 +150,8 @@ void setup() {
 
     gfx->setBrightness(100);
     gfx->fillScreen(RGB565_BLACK);
-    draw_logo();
+    
+    //draw_logo();
 
     WiFi.mode(WIFI_STA);
     while (WiFi.status()==WL_STOPPED){}
@@ -198,9 +208,10 @@ void setup() {
     Wire.setPins(IIC_SDA, IIC_SCL);
     Wire.begin();
 
-    delay(2000);    // small display so we can see the logo :-)
+    // delay(2000);    // small display so we can see the logo :-)
+    // draw_display();    
 
-    draw_display();    
+    draw_speedometer_display();
 
     inactive_timer = millis();  // start inactivity timer 
 }
@@ -213,7 +224,8 @@ void loop() {
         esp_now_message.status = OFFLINE;
         
         // Draw screen to show offline
-        draw_display();
+        //draw_display();
+        draw_speedometer_display();
         
         // Reset inactive time just in case it come back
         inactive_timer = millis();
@@ -221,61 +233,24 @@ void loop() {
 }
 
 /**
- * Draw splash-screen at start of program
+ * @brief Draw the speedometer inspired screeen
  */
-void draw_logo(void) {
+void draw_speedometer_display(void) {
     sprite.fillSprite(0);
-    
-    sprite.setFreeFont(FSB24);     
-    sprite.setTextColor(RGB565_TEAL);
-    sprite.drawString("Audio", 161, 167);
-    sprite.drawString("Guestbook", 123, 250);
-    sprite.unloadFont();
+
+    draw_recordings();
+    draw_diskspace();
+    draw_status_icon();
 
     gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t*)sprite.getPointer(), 466, 466);  
+    sprite.setTextSize(0);    
 }
 
-/**
- * @brief Draw the screeen
- */
-void draw_display(void) {
-    sprite.fillSprite(0);
-    sprite.fillRoundRect(177, 6, 6, 460, 4, RGB565_WHITE);
-    
-    // sprite.loadFont(valueFont);
-    sprite.setFreeFont(FSB24);     
-    sprite.setTextColor(RGB565_TEAL);
-    sprite.drawString("Audio", 23, 140);
-    sprite.drawString("Guest", 33, 200);
-    sprite.drawString("Book", 43, 260);
-    sprite.unloadFont();
-
-    sprite.setFreeFont(FSSB18);
-    sprite.setTextColor(RGB565_WHITE);
-    sprite.drawString("Status", 207, 30);
-    sprite.drawString("Recordings", 207, 140);
-    sprite.drawString("Disk Space", 207, 255);
-    sprite.drawString("Up Time", 207, 365);
-    sprite.unloadFont();
-
-    // sprite.loadFont(middleFont);
-    sprite.setFreeFont(FSS18);     
-    sprite.setTextColor(RGB565_LIGHTGREY);
-
-    uint16_t status_colour = update_status(esp_now_message.status);
-    sprite.setTextColor(status_colour);
-    sprite.drawString(phone_status, 207, 70);
-    memset(phone_status, '\0', sizeof(phone_status));
-
-    // Reset colour just in case status changed it
-    sprite.setTextColor(RGB565_LIGHTGREY);
-
-    char recordings_buffer[10];
-    sprintf(recordings_buffer, "%u", esp_now_message.recordings);
-    sprite.drawString(recordings_buffer, 207, 180);
+void draw_diskspace(void) {
+    sprite.drawArc(233, 420, 90, 92, 90, 270, RGB565_WHITE, RGB565_BLACK, true);
 
     uint64_t bytes = esp_now_message.disk_space;
-    double humanBytes = 0;
+    float humanBytes = 0;
     int i = 0;
 	char *suffix[] = {"B", "KB", "MB", "GB", "TB"};
     char length = sizeof(suffix) / sizeof(suffix[0]);
@@ -284,18 +259,245 @@ void draw_display(void) {
 			humanBytes = bytes / 1024.0;
     }
 
+    sprite.drawWideLine(161, 379, 172, 386, 4, RGB565_WHITE);   // 16gb
+    sprite.drawWideLine(191, 348, 198, 361, 4, RGB565_WHITE);   // 12gb
+    sprite.drawWideLine(233, 338, 233, 352, 4, RGB565_WHITE);   // 8gb
+    sprite.drawWideLine(274, 348, 267, 361, 4, RGB565_WHITE);   // 4gb
+    sprite.drawWideLine(303, 374, 291, 384, 4, RGB565_RED);     // 0gb
+
     char diskspace_buffer[25];
     sprintf(diskspace_buffer, "%.02lf %s", humanBytes, suffix[i]);
-    sprite.drawString(diskspace_buffer, 207, 295);
 
-    char uptime_buffer[10];
-    sprintf(uptime_buffer, "%02d:%02d:%02d", (esp_now_message.last_time / 1000) / 3600, ((esp_now_message.last_time / 1000) % 3600) / 60,
-            ((esp_now_message.last_time / 1000) % 3600) % 60);
-    sprite.drawString(uptime_buffer, 207, 405);
-    sprite.unloadFont();
+    if (humanBytes > 15.5)
+        sprite.drawWedgeLine(233, 430, 174, 387, 5, 1, RGB565_WHITE);   // 16gb
+    else if (humanBytes > 14.5)
+        sprite.drawWedgeLine(233, 430, 180, 381, 5, 1, RGB565_WHITE);   // 15gb
+    else if (humanBytes > 13.5)
+        sprite.drawWedgeLine(233, 430, 186, 377, 5, 1, RGB565_WHITE);   // 14gb
+    else if (humanBytes > 12.5)
+        sprite.drawWedgeLine(233, 430, 191, 370, 5, 1, RGB565_WHITE);   // 13gb
+    else if (humanBytes)
+        sprite.drawWedgeLine(233, 430, 200, 365, 5, 1, RGB565_WHITE);   // 12gb
+    else if (humanBytes > 10.5)
+        sprite.drawWedgeLine(233, 430, 208, 362, 5, 1, RGB565_WHITE);   // 11gb
+    else if (humanBytes > 9.5)
+        sprite.drawWedgeLine(233, 430, 217, 358, 5, 1, RGB565_WHITE);   // 10gb
+    else if (humanBytes > 8.5)
+        sprite.drawWedgeLine(233, 430, 225, 358, 5, 1, RGB565_WHITE);   // 9gb
+    else if (humanBytes > 7.5)
+        sprite.drawWedgeLine(233, 430, 233, 358, 5, 1, RGB565_WHITE);   // 8gb
+    else if (humanBytes > 6.5)
+        sprite.drawWedgeLine(233, 430, 240, 358, 5, 1, RGB565_WHITE);   // 7gb
+    else if (humanBytes > 5.5)
+        sprite.drawWedgeLine(233, 430, 247, 359, 5, 1, RGB565_WHITE);   // 6gb
+    else if (humanBytes > 4.5)
+        sprite.drawWedgeLine(233, 430, 256, 361, 5, 1, RGB565_WHITE);   // 5gb
+    else if (humanBytes > 3.5)
+        sprite.drawWedgeLine(233, 430, 264, 364, 5, 1, RGB565_WHITE);   // 4gb
+    else if (humanBytes > 2.5)
+        sprite.drawWedgeLine(233, 430, 269, 369, 5, 1, RGB565_WHITE);   // 3gb
+    else if (humanBytes > 1.5)
+        sprite.drawWedgeLine(233, 430, 275, 373, 5, 1, RGB565_WHITE);   // 2gb
+    else if (humanBytes > 0.5)
+        sprite.drawWedgeLine(233, 430, 281, 379, 5, 1, RGB565_ORANGE);   // 1gb
+    else if (humanBytes <= 0)
+        sprite.drawWedgeLine(233, 430, 286, 386, 5, 1, RGB565_RED);   // 0gb
 
-    gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t*)sprite.getPointer(), 466, 466);  
+    sprite.setTextColor(RGB565_WHITE);
+    sprite.setFreeFont(FM9);
+
+    if (humanBytes > 10) {
+        sprite.drawString(diskspace_buffer, 195, 445);
+    } else {
+        sprite.drawString(diskspace_buffer, 203, 445);
+    }
 }
+
+/**
+ * @brief Draw the recordings arc and number of recordings
+ */
+void draw_recordings(void) {
+    const int CENTER_X = 233;
+    const int CENTER_Y = 233;
+    const int RADIUS = 233;
+    const int WIDTH = 38;
+    const int START_ANGLE = 80;
+    const int END_ANGLE = 280;
+    const int RECORDINGS_Y = 200;
+    int recordings_x = 0;
+    int recordings_angle = START_ANGLE;  // no recordings, calculate angle from recordings
+
+    char recordings_buffer[10];
+
+    sprite.setTextColor(RGB565_WHITE);
+    sprite.setFreeFont(FM9);
+    sprite.setCursor(50, 268); sprite.print("0");
+
+    // 200 ticks in the arc
+    if (esp_now_message.recordings < 60) {
+        recordings_angle = START_ANGLE + esp_now_message.recordings;
+        sprite.setCursor(388, 268); sprite.print("100");
+    } else if (esp_now_message.recordings < 290) {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 2);
+        sprite.setCursor(388, 268); sprite.print("400");
+    } else if (esp_now_message.recordings < 700) {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 4);
+        sprite.setCursor(388, 268); sprite.print("800");
+    } else if (esp_now_message.recordings < 1500) {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 8);
+        sprite.setCursor(375, 268); sprite.print("1600");
+    } else if (esp_now_message.recordings < 3100) {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 16);
+        sprite.setCursor(375, 268); sprite.print("3200");
+    } else if (esp_now_message.recordings < 6200) {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 32);
+        sprite.setCursor(375, 268); sprite.print("6400");
+    } else if (esp_now_message.recordings < 12700) {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 64);
+        sprite.setCursor(363, 268); sprite.print("12800");
+    } else {
+        recordings_angle = (int) START_ANGLE + (esp_now_message.recordings / 128);
+        sprite.setTextColor(RGB565_BLACK);
+        sprite.setCursor(50, 268); sprite.print(" ");
+    }
+
+    // saftey for my maths being wrong!
+    if (recordings_angle > END_ANGLE) {
+        recordings_angle = END_ANGLE - 10;
+    }
+    
+    sprite.drawArc(CENTER_X, CENTER_Y, RADIUS, RADIUS - WIDTH, START_ANGLE, END_ANGLE, RGB565_BACKGROUND, RGB565_BLACK, true);
+    sprite.drawArc(CENTER_X, CENTER_Y, RADIUS, RADIUS - WIDTH, START_ANGLE, recordings_angle, RGB565_WHITE, RGB565_BLACK, true);
+
+    // Centre line for drawing guidance
+    // sprite.drawLine(0, CENTER_Y, 466, CENTER_Y, RGB565_RED);
+    // sprite.drawLine(CENTER_X, 0, CENTER_X, 466, RGB565_RED);
+    
+    sprite.setTextColor(RGB565_TEAL);
+    sprite.setFreeFont(&FreeSansBold18pt7b);
+    sprite.setCursor(185, 105); sprite.print("Audio");
+    sprite.setCursor(142, 150); sprite.print("Guestbook");
+
+    sprite.setTextColor(RGB565_WHITE);
+    sprite.setFreeFont(FM24);
+    sprite.setTextSize(2);
+
+    sprintf(recordings_buffer, "%u", esp_now_message.recordings);
+
+    if (esp_now_message.recordings < 10) {
+        recordings_x = 205;
+    } else if (esp_now_message.recordings < 100) {
+        recordings_x = 177;
+    } else if (esp_now_message.recordings < 1000) {
+        recordings_x = 149;
+    } else {
+        recordings_x = 122;
+    }
+
+    sprite.drawString(recordings_buffer, recordings_x, RECORDINGS_Y);
+
+    // Reset font size
+    sprite.setTextSize(0);
+}
+
+/**
+ * @brief Draw the status icon, get colour from the status function
+ */
+void draw_status_icon(void) {
+    const int FIRST_LINE_X = 330;
+    const int FIRST_LINE_Y = 320;
+    const int LINE_HEIGHT = 20;
+    const int LINE_WIDTH = 3;
+    const int LINE_GAP = 8;
+
+    uint16_t status_colour = update_status(esp_now_message.status);
+
+    // Draw icon, starting on the left
+    sprite.drawWideLine(FIRST_LINE_X, FIRST_LINE_Y, FIRST_LINE_X, FIRST_LINE_Y + LINE_HEIGHT, LINE_WIDTH, status_colour);
+    sprite.drawWideLine(FIRST_LINE_X + LINE_GAP, FIRST_LINE_Y - 10, FIRST_LINE_X + LINE_GAP, FIRST_LINE_Y + LINE_HEIGHT + 10, LINE_WIDTH, status_colour);
+    sprite.drawWideLine(FIRST_LINE_X + (LINE_GAP * 2), FIRST_LINE_Y - 20, FIRST_LINE_X + (LINE_GAP * 2), FIRST_LINE_Y + LINE_HEIGHT + 20, LINE_WIDTH, status_colour);
+    sprite.drawWideLine(FIRST_LINE_X + ((LINE_GAP * 4) - 5), FIRST_LINE_Y - 15, FIRST_LINE_X + ((LINE_GAP * 4) - 5), FIRST_LINE_Y + LINE_HEIGHT + 15, LINE_WIDTH, status_colour);
+    sprite.drawWideLine(FIRST_LINE_X + ((LINE_GAP * 5) - 5), FIRST_LINE_Y - 25, FIRST_LINE_X + ((LINE_GAP * 5) - 5), FIRST_LINE_Y + LINE_HEIGHT + 25, LINE_WIDTH, status_colour);
+    sprite.drawWideLine(FIRST_LINE_X + ((LINE_GAP * 6) - 5), FIRST_LINE_Y - 10, FIRST_LINE_X + ((LINE_GAP * 6) - 5), FIRST_LINE_Y + LINE_HEIGHT + 10, LINE_WIDTH, status_colour);
+    sprite.drawWideLine(FIRST_LINE_X + ((LINE_GAP * 7) - 5), FIRST_LINE_Y, FIRST_LINE_X + ((LINE_GAP * 7) - 5), FIRST_LINE_Y + LINE_HEIGHT, LINE_WIDTH, status_colour);
+}
+
+// /**
+//  * Draw splash-screen at start of program
+//  */
+// void draw_logo(void) {
+//     sprite.fillSprite(0);
+    
+//     sprite.setFreeFont(FSB24);     
+//     sprite.setTextColor(RGB565_TEAL);
+//     sprite.drawString("Audio", 161, 167);
+//     sprite.drawString("Guestbook", 123, 250);
+//     sprite.unloadFont();
+
+//     gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t*)sprite.getPointer(), 466, 466);  
+// }
+
+// /**
+//  * @brief Draw the screeen
+//  */
+// void draw_display(void) {
+//     sprite.fillSprite(0);
+//     sprite.fillRoundRect(177, 6, 6, 460, 4, RGB565_WHITE);
+    
+//     // sprite.loadFont(valueFont);
+//     sprite.setFreeFont(FSB24);     
+//     sprite.setTextColor(RGB565_TEAL);
+//     sprite.drawString("Audio", 23, 140);
+//     sprite.drawString("Guest", 33, 200);
+//     sprite.drawString("Book", 43, 260);
+//     sprite.unloadFont();
+
+//     sprite.setFreeFont(FSSB18);
+//     sprite.setTextColor(RGB565_WHITE);
+//     sprite.drawString("Status", 207, 30);
+//     sprite.drawString("Recordings", 207, 140);
+//     sprite.drawString("Disk Space", 207, 255);
+//     sprite.drawString("Up Time", 207, 365);
+//     sprite.unloadFont();
+
+//     // sprite.loadFont(middleFont);
+//     sprite.setFreeFont(FSS18);     
+//     sprite.setTextColor(RGB565_LIGHTGREY);
+
+//     uint16_t status_colour = update_status(esp_now_message.status);
+//     sprite.setTextColor(status_colour);
+//     sprite.drawString(phone_status, 207, 70);
+//     memset(phone_status, '\0', sizeof(phone_status));
+
+//     // Reset colour just in case status changed it
+//     sprite.setTextColor(RGB565_LIGHTGREY);
+
+//     char recordings_buffer[10];
+//     sprintf(recordings_buffer, "%u", esp_now_message.recordings);
+//     sprite.drawString(recordings_buffer, 207, 180);
+
+//     uint64_t bytes = esp_now_message.disk_space;
+//     double humanBytes = 0;
+//     int i = 0;
+// 	char *suffix[] = {"B", "KB", "MB", "GB", "TB"};
+//     char length = sizeof(suffix) / sizeof(suffix[0]);
+//     if (esp_now_message.disk_space > 1024) {
+// 		for (i = 0; (bytes / 1024) > 0 && i<length-1; i++, bytes /= 1024)
+// 			humanBytes = bytes / 1024.0;
+//     }
+
+//     char diskspace_buffer[25];
+//     sprintf(diskspace_buffer, "%.02lf %s", humanBytes, suffix[i]);
+//     sprite.drawString(diskspace_buffer, 207, 295);
+
+//     char uptime_buffer[10];
+//     sprintf(uptime_buffer, "%02d:%02d:%02d", (esp_now_message.last_time / 1000) / 3600, ((esp_now_message.last_time / 1000) % 3600) / 60,
+//             ((esp_now_message.last_time / 1000) % 3600) % 60);
+//     sprite.drawString(uptime_buffer, 207, 405);
+//     sprite.unloadFont();
+
+//     gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t*)sprite.getPointer(), 466, 466);  
+// }
 
 /**
  * @brief Receive data from ESP-NOW server callback and update the display
@@ -313,7 +515,8 @@ void esp_now_data_recv(const esp_now_recv_info_t *info, const uint8_t *incoming_
     esp_now_message.status = data->status;
     esp_now_message.recordings = data->recordings;
 
-    draw_display();
+    //draw_display();
+    draw_speedometer_display();
 
     // When we get a message from the phone reset the inactive_timer to now
     inactive_timer = millis();    
@@ -325,7 +528,7 @@ void esp_now_data_recv(const esp_now_recv_info_t *info, const uint8_t *incoming_
  * to draw the text depending on status
  */
  static uint16_t update_status(const uint8_t status) {
-    uint16_t status_colour = RGB565_LIGHTGREY;
+    uint16_t status_colour = RGB565_BACKGROUND;
 
     switch (status) {
         case ERROR:
@@ -346,7 +549,7 @@ void esp_now_data_recv(const esp_now_recv_info_t *info, const uint8_t *incoming_
 
         case RECORDING:
             sprintf(phone_status, "Recording");
-            status_colour = RGB565_GREEN;
+            status_colour = RGB565_LAWNGREEN;
             break;
 
         case PLAYING:
